04fc60c964f297d33550cd62c292e3af02e3810d
Mohammad Yusif <mosefbel@gmail.com>
2026-01-29 20:52:59 +0300

    Fix API deployment: compile TypeScript on EB and update Procfile
    - Update Procfile to run from www folder: node www/server.js
    - Add postinstall script to compile TypeScript on EB
    - Move typescript to dependencies for EB installation

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -3,5 +3,7 @@
 # Set environment variables on Elastic Beanstalk
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION JWT_SECRET=$JWT_SECRET URL=$URL --environment udagram-api-dev
 
-# Deploy to Elastic Beanstalk
-eb deploy udagram-api-dev
+# Deploy the built archive from www folder
+cd www
+eb deploy udagram-api-dev --staged
+cd ..
